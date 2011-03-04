17ec7710613010ce8bdc7376cedf4f134aa5b257
brianaxelrod <brianaxelrod@8c3036a0-8766-4673-8ad9-b43f491d8da9>
2011-03-04 23:44:02 +0000

    Added line following. -BA

---
+++ LRT11/Components/LineSensor.h
@@ -0,0 +1,34 @@
+#ifndef LINE_SENSOR_H
+#define LINE_SENSOR_H
+#include "Component.h"
+#include "..\Config\Configurable.h"
+#include "..\Config\Config.h"
+
+class LineSensor : public Component, public Configurable
+{
+public:
+    LineSensor(int adcPort, int clockOut, int siOut);
+    virtual ~LineSensor();
+
+    float GetLinePosition();
+private:
+    AnalogChannel analogIn;
+    DigitalOutput clockOut, siOut;
+    Config& config;
+
+    const static int NUM_PIXELS = 128;
+    float lastLinePos;
+    unsigned int pixelData[NUM_PIXELS];
+    unsigned int exposure, noiseThreshold, lineThreshold;
+
+    void UpdateReadings();
+    void SetExpo(unsigned int exposure);
+    void Flush();
+    void ResetSensor();
+    void PrepFrame();
+
+    inline void DelayMicroSeconds(int ms);
+
+};
+
+#endif
